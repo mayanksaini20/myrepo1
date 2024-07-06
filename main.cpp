9f35332e64637a7cd66180a7cea5db8607b68831
mayanksaini20 <mayanksainisre79@gmail.com>
2024-07-06 10:24:59 +0530

    2nd commit main.cpp file

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "hello world ";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "fuck you 🤘";
+    }
+
+    return 0;
+}
